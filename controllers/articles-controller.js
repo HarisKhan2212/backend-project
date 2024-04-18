052783411a7ee0b6a07e1b1542a71db679a1b65f
hariskhan2212 <hariskhan2122@outlook.com>
2024-04-18 10:33:09 +0100

    completed task 5 adding endpoint /articles to give all articles in correct format as well as sorting for most recent comments by date

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -1,24 +1,4 @@
-// const { articleByArticleId } = require("../models/articles-model")
 
-// exports.getArticleById = (req, res, next) => {
-//     const { article_id } = req.params
-//     return articleByArticleId(article_id).then((article) => {
-//         res.status(200).send({ article })
-//     })
-//     .catch(next)
-// }
-
-// exports.getArticleById = (req, res, next) => {
-//     const { article_id } = req.params
-//     return articleByArticleId(article_id)
-//         .then((article) => {
-//             if (!article) {
-//                 return res.status(404).send({ msg: 'Article not found' });
-//             }
-//             res.status(200).send({ article });
-//         })
-//         .catch(next);
-// };
 const { selectArticleById } = require("../models/articles-model")
 
 exports.getArticleById = (req, res, next) => {
@@ -28,3 +8,10 @@ exports.getArticleById = (req, res, next) => {
     })
     .catch(next)
 } 
+
+exports.getArticles = (req, res, next) => {
+    return selectArticles().then((articles) => {
+        res.status(200).send({ articles })
+    })
+    .catch(next)
+}
